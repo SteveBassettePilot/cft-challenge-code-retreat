314da03bfb58461d2d62c44df13c989f562827e5
Steve Bassette <steven.bassette@pilottravelcenters.com>
2022-08-17 16:35:34 -0400

    Adds CLI Switches to choose what to build

--- make.sh
+++ make.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 
+BUILD_S3=false
+PACKAGE=false
+DEPLOY=false
+
 for i in "$@"; do
   case $i in
     -p=*|--prefix=*)
       PREFIX="${i#*=}"
+      shift # past argument=value
+      ;;
+    -S|--S3)
+      BUILD_S3=true
+      shift # past argument=value
+      ;;
+    -P|--package)
+      PACKAGE=true
+      shift # past argument=value
+      ;;
+    -D|--deploy)
+      DEPLOY=true
       shift # past argument=value
       ;;
     -*|--*)
@@ -17,28 +33,34 @@ done
 
 REGION="us-west-2"
 
+
 set -x
 
-echo "Create Deployment Stack: $PREFIX-deploy"
-aws --region $REGION \
-    cloudformation deploy \
-    --template-file "deploy/deployment-s3.yaml" \
-    --stack-name "$PREFIX-deploy"  \
-    --parameter-overrides Prefix="$PREFIX"
+if [ "$BUILD_S3" = "true" ] ; then
+  echo "Create Deployment Stack: $PREFIX-deploy"
+  aws --region $REGION \
+      cloudformation deploy \
+      --template-file "deploy/deployment-s3.yaml" \
+      --stack-name "$PREFIX-deploy"  \
+      --parameter-overrides Prefix="$PREFIX"
+fi
 
-echo "Packaging Component Infrastructure"
-aws --region $REGION \
-    cloudformation package \
-    --template-file infra/component.yaml \
-    --output-template packaged.yaml \
-    --s3-bucket "$PREFIX-deployment-s3"
+if [ "$PACKAGE" = "true" ] ; then
+  echo "Packaging Component Infrastructure"
+  aws --region $REGION \
+      cloudformation package \
+      --template-file infra/component.yaml \
+      --output-template packaged.yaml \
+      --s3-bucket "$PREFIX-deployment-s3"
+fi
 
-echo "Creating Component Infrastructure"
-aws --region $REGION \
-    cloudformation deploy \
-    --template-file packaged.yaml \
-    --stack-name "$PREFIX-challenge"  \
-    --parameter-overrides Prefix="$PREFIX"
-
+if [ "$DEPLOY" = "true" ] ; then
+  echo "Creating Component Infrastructure"
+  aws --region $REGION \
+      cloudformation deploy \
+      --template-file packaged.yaml \
+      --stack-name "$PREFIX-challenge"  \
+      --parameter-overrides Prefix="$PREFIX"
+fi
 
 set +x
