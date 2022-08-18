0237e7d5c648756f22d9e2b63fac4689ce146d6f
Steve Bassette <steven.bassette@pilottravelcenters.com>
2022-08-18 07:05:09 -0400

    Adds some help for the script

--- make.sh
+++ make.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+PREFIX=
 BUILD_S3=false
 PACKAGE=false
 DEPLOY=false
@@ -8,6 +9,12 @@ for i in "$@"; do
   case $i in
     -p=*|--prefix=*)
       PREFIX="${i#*=}"
+      shift # past argument=value
+      ;;
+    -C|--complete)
+      BUILD_S3=true
+      PACKAGE=true
+      DEPLOY=true
       shift # past argument=value
       ;;
     -S|--S3)
@@ -22,14 +29,47 @@ for i in "$@"; do
       DEPLOY=true
       shift # past argument=value
       ;;
+    -h|--help)
+      echo "This app is part of a challenge to create a working CFT Structure"
+      echo ""
+      echo "USAGE:"
+      echo "  ./make.sh -p=<team-name> [OPTIONS]"
+      echo ""
+      echo "OPTIONS:"
+      echo "  -p|--prefix"
+      echo "      REQUIRED"
+      echo "      The team name prefix to be used for all CFTs"
+      echo "  "
+      echo "  -C|--complete"
+      echo "      Run all steps of the challenge deployment"
+      echo "      Functions the same as passing the -S -P -D options together"
+      echo "  "
+      echo "  -S|--S3"
+      echo "      Create an S3 Bucket to stage the team's deployment items to"
+      echo "  "
+      echo "  -P|--package"
+      echo "      Package the Component infrastructure and stage them for deploy"
+      echo "  "
+      echo "  -D|--deploy"
+      echo "      Deploy the Component to AWS and build the stacks"
+      exit 1
+      ;;
     -*|--*)
       echo "Unknown option $i"
+      echo "Try the -h option for help"
       exit 1
       ;;
     *)
       ;;
   esac
 done
+
+if [ "$PREFIX" = "" ] ; then
+  echo "Prefix is required"
+  echo "Try the -h option for help"
+  exit 1
+fi
+
 
 REGION="us-west-2"
 
